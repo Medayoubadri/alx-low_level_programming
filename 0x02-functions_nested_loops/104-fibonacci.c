5e77343eb47905ec3a07c57e89d45c7b6f4d9f7d
medayoubadri <medayoubadri@gmail.com>
2024-06-26 00:38:39 +0100

    Add program to print the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,15 +3,18 @@
 #define LARGEST 10000000000
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
- * Return: Always 0 (Success)
- */
+* main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 unsigned long fib1 = 1, fib2 = 2;
+
 unsigned long fib1_high, fib1_low, fib2_high, fib2_low;
+
 unsigned long high, low;
+
 int count;
 
 printf("%lu, %lu", fib1, fib2);
@@ -19,6 +22,7 @@ printf("%lu, %lu", fib1, fib2);
 for (count = 2; count < 92; count++)
 {
 	unsigned long next = fib1 + fib2;
+
 	printf(", %lu", next);
 	fib1 = fib2;
 	fib2 = next;
@@ -48,4 +52,3 @@ for (count = 92; count < 98; count++)
 printf("\n");
 return (0);
 }
-
