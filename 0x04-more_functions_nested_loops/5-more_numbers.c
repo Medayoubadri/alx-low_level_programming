299a484d09950a1fc2d3e15c82efc4371ad85863
medayoubadri <medayoubadri@gmail.com>
2024-06-28 14:55:37 +0100

    Task 5: Add function to print 10 times the numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
 /**
- * more_numbers prints 10 times the numbers, from 0 to 14
+ * more_numbers - prints the numbers, from 0 to 14, 10 times
+ *
+ * Description: This function uses nested loops to print numbers from 0 to 14,
+ *              ten times, each followed by a new line. It's like a counting
+ *              marathon, but way more fun!
+ *
+ * Return: from 0 to 14, 10 times, followed by a new line.
  */
 void more_numbers(void)
 {
@@ -12,8 +18,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
