b3cae23294838e552c95a55778cb4161bb4e16b6
medayoubadri <medayoubadri@gmail.com>
2024-07-05 01:07:52 +0100

    Task 0: Correct comment style to C-style comments

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Description: This function appends the src string to the dest string,
+ *              overwriting the terminating null byte (\0) at the end of dest,
+ *              and then adds a terminating null byte.
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	/* Find the end of dest string */
+	while (dest[i] != '\0')
+		i++;
+
+	/* Append src to dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add terminating null byte */
+	dest[i] = '\0';
+
+	return (dest);
+}
+
