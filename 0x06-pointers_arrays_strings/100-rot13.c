4f9f4cb301c63bd94ceb08c53f33ca2837ce1b6e
medayoubadri <medayoubadri@gmail.com>
2024-07-05 01:58:30 +0100

    Task 8: Add function to encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: pointer to the string to be encoded
+ *
+ * Description: This function encodes a string using rot13, a simple
+ *              substitution cipher that replaces a letter with 
+ *              the letter 13 letters after it in the alphabet.
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
