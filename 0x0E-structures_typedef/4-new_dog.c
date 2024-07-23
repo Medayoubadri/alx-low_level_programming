7c26a42f9ee8a2e81e5d17f0e89c4b54de8e7000
medayoubadri <medayoubadri@gmail.com>
2024-07-23 12:39:46 +0100

    Task 4: Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,31 +3,30 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dag.
- * @name: The name of the dowg.
- * @age: The age of the dawg.
- * @owner: The dog of the owner.
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: This function creates a new dog and stores copies of the name
- *              and owner.
- * Return: A pointer to the new dog, or NULL if it fails.
+ * Description: This function creates a new dog and stores a copy of
+ * the name and owner. Returns NULL if the function fails.
+ * Every dog deserves a fresh start, don't they?
+ *
+ * Return: Pointer to the new dog (dog_t), or NULL if it fails.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *thedog;
 	char *new_name, *new_owner;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	thedog = malloc(sizeof(dog_t));
+	if (thedog == NULL)
 		return (NULL);
 
 	new_name = malloc(strlen(name) + 1);
 	if (new_name == NULL)
 	{
-		free(new_dog);
+		free(thedog);
 		return (NULL);
 	}
 	strcpy(new_name, name);
@@ -36,15 +35,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_owner == NULL)
 	{
 		free(new_name);
-		free(new_dog);
+		free(thedog);
 		return (NULL);
 	}
 	strcpy(new_owner, owner);
 
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
+	thedog->name = new_name;
+	thedog->age = age;
+	thedog->owner = new_owner;
 
-	return (new_dog);
+	return (thedog);
 }
 
