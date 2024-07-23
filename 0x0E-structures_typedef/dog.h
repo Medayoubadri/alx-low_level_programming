4b313bdf0765135368cde8daf4e47c87d63eefc5
medayoubadri <medayoubadri@gmail.com>
2024-07-23 09:56:46 +0100

    Task 0: Define a new type struct dog with name, age, and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dowg.
+ * @age: The age of the dawg.
+ * @owner: The dog of the owner.
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
