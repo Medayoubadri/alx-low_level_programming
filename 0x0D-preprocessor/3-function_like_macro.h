221aa58861cac96c74555f1af73dd0a6cd80df71
medayoubadri <medayoubadri@gmail.com>
2024-07-23 01:45:41 +0100

    Task 4: Create a function-like macro SUM(x, y) that computes the sum of the numbers x and y

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /* ABS, If you don't have them you'll never be able to code */
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? -1 * (x) : (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
 
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef SUM_H
+#define SUM_H
+
+/* You can SUM the fu**k up... */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
+
