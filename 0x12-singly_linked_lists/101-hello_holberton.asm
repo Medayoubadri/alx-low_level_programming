7402d396e41d09ca757cc50488f64470b645ff96
medayoubadri <medayoubadri@gmail.com>
2024-08-01 18:41:27 +0100

    Task 6: Write a 64-bit assembly program to print Hello, Holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,11 @@
 section .data
-    msg db "Hello, Holberton", 0   ; Message to print, who is this hoberton guy!! it's "hello, world!"
+    msg db "Hello, Holberton", 0
 
 section .text
     global main
 
 main:
-    ; Load the message address into the first argument for printf
     mov rdi, msg
-    ; Call printf function
     call printf
-    ; Exit the program gracefully
     ret
 
