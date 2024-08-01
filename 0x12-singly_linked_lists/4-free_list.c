9c1b86128dfdf23855421cce2473c0445668931a
medayoubadri <medayoubadri@gmail.com>
2024-08-01 17:26:41 +0100

    Task 4: Write a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ *
+ * Description: Free your list and free your mind. 
+ * It's time to take the red pill and let go.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
