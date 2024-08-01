84b93d81f0ed42d8238976f463e15720e7ec8aa6
medayoubadri <medayoubadri@gmail.com>
2024-08-01 18:47:16 +0100

    Task 6: Write a 64-bit assembly program to print Hello, Holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,14 @@
 section .data
-    msg db "Hello, Holberton", 0
+    format db "Hello, Holberton", 10, 0  ; Message with newline, it shoudl be "hello, world!"...
 
 section .text
     global main
+    extern printf
 
 main:
-    mov rdi, msg
-    call printf
+    mov rdi, format   ; First argument: pointer to format string
+    xor eax, eax      ; Zero out eax, as printf is a variadic function
+    call printf       ; Call printf
+    mov eax, 0        ; Return 0 status
     ret
 
