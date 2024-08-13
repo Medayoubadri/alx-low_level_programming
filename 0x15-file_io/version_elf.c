35c7fb10137a810d7672b6112c0d7f6029642519
medayoubadri <medayoubadri@gmail.com>
2024-08-13 16:05:41 +0100

    Task 4: ELF header reader implemented.

--- 0x15-file_io/version_elf.c
+++ 0x15-file_io/version_elf.c
@@ -8,16 +8,19 @@
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("  Version:                           %d",
-	       e_ident[EI_VERSION]);
+	printf("  Version:                           %d", e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
 		printf(" (current)\n");
 		break;
+	case 2:
+		printf(" (non-standard: modified for testing)\n");
+		break;
 	default:
-		printf("\n");
+		printf(" (unknown version)\n");
 		break;
 	}
 }
+
