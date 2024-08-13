65fe2e8961e23d9af75ee9acbb7ff8fd02d09bfe
medayoubadri <medayoubadri@gmail.com>
2024-08-13 19:17:58 +0100

    Task 4: ELF header reader implemented.

--- 0x15-file_io/version_elf.c
+++ 0x15-file_io/version_elf.c
@@ -4,7 +4,8 @@
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  *
- * Description: Checking the version, & hoping it’s not outdated like my jokes.
+ * Description: Checking the version, & hoping it’s not outdated
+ *              like my jokes.
  */
 void print_version(unsigned char *e_ident)
 {
