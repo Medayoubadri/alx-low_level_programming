70b53729ea3278424dfc07e2ceb7c66f0086e206
medayoubadri <medayoubadri@gmail.com>
2024-09-24 00:18:02 +0100

    Make me win the Giga Millions jackpot using LD_PRELOAD

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Medayoubadri/alx-low_level_programming/raw/main/0x18-dynamic_libraries/Betting_Regrets.so
-export LD_PRELOAD="$PWD/../Betting_Regrets.so"
+wget -P /tmp https://github.com/Medayoubadri/alx-low_level_programming/raw/main/0x18-dynamic_libraries/Betting_Regrets.so
+export LD_PRELOAD=/tmp/Betting_Regrets.so
