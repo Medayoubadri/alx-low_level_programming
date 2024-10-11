45bae4c1726532895609f7f641d65ff326c05cfc
medayoubadri <medayoubadri@gmail.com>
2024-10-11 18:44:22 +0100

    Task 0: Create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it fails.
+ *
+ * When you create a hash table in C, it's like assembling IKEA furniture.
+ * You follow the instructions, but deep down, you're wondering
+ * where that extra screw came from, and the whole thing collapses in a month.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	/* Allocate memory for the hash table itself */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/* Allocate memory for the array of pointers */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* Initialize all pointers in the array to NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	new_table->size = size;
+
+	return (new_table);
+}
